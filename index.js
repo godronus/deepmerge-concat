142d959498f36c7489b527625de25f281e32617d
Gordon <godronus@yahoo.com>
2019-05-29 12:48:13 +0100

    master

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const deepmerge = require('deepmerge');
 
+const deepConcat = new Object(deepmerge);
+
 function isObject(obj) {
   return (obj !== null && typeof obj === 'object');
 }
@@ -69,7 +71,7 @@ const cleanSourceMap = (dirty, clean) => {
   }, {});
 };
 
-deepmerge.constructor.prototype.concat = function queries(target, source) {
+deepConcat.concat = function queries(target, source) {
   if (!isObject(target) || !isObject(source)) return new Error('Must recieve two objects to merge');
   if (Array.isArray(target) || Array.isArray(source)) return new Error('Must recieve two objects to merge');
   const { queryObj, containsArrays } = queryStrToArr(target);
@@ -86,5 +88,5 @@ deepmerge.constructor.prototype.concat = function queries(target, source) {
   return queryArrToStr(mergedQueries);
 };
 
-module.exports = deepmerge;
+module.exports = deepConcat;
 
